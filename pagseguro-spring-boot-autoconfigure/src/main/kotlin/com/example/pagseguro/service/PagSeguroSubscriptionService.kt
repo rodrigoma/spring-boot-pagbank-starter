0c267b79da6c3374185aeb5efd362e78ea3c0baa
Rodrigo Monteiro <rodrigoma@gmail.com>
2026-03-25 23:59:51 -0300

    feat: implement PagSeguroSubscriptionService with DTOs

--- pagseguro-spring-boot-autoconfigure/src/main/kotlin/com/example/pagseguro/service/PagSeguroSubscriptionService.kt
+++ pagseguro-spring-boot-autoconfigure/src/main/kotlin/com/example/pagseguro/service/PagSeguroSubscriptionService.kt
@@ -1,3 +1,48 @@
 package com.example.pagseguro.service
+
+import com.example.pagseguro.model.subscription.*
 import org.springframework.web.client.RestClient
-class PagSeguroSubscriptionService(private val restClient: RestClient)
+import org.springframework.web.client.body
+
+class PagSeguroSubscriptionService(private val restClient: RestClient) {
+
+    fun create(request: CreateSubscriptionRequest): SubscriptionResponse =
+        restClient.post()
+            .uri("/subscriptions")
+            .body(request)
+            .retrieve()
+            .body<SubscriptionResponse>()!!
+
+    fun get(id: String): SubscriptionResponse =
+        restClient.get()
+            .uri("/subscriptions/{id}", id)
+            .retrieve()
+            .body<SubscriptionResponse>()!!
+
+    fun cancel(id: String) {
+        restClient.put()
+            .uri("/subscriptions/{id}/cancel", id)
+            .retrieve()
+            .toBodilessEntity()
+    }
+
+    fun suspend(id: String) {
+        restClient.put()
+            .uri("/subscriptions/{id}/suspend", id)
+            .retrieve()
+            .toBodilessEntity()
+    }
+
+    fun reactivate(id: String) {
+        restClient.put()
+            .uri("/subscriptions/{id}/reactivate", id)
+            .retrieve()
+            .toBodilessEntity()
+    }
+
+    fun list(): SubscriptionListResponse =
+        restClient.get()
+            .uri("/subscriptions")
+            .retrieve()
+            .body<SubscriptionListResponse>()!!
+}
