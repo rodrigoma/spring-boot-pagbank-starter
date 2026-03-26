7fcad0c72349fdaf92d31e8a595abb1b5bcaa684
Rodrigo Monteiro <rodrigoma@gmail.com>
2026-03-26 00:02:39 -0300

    feat: implement PagSeguroRefundService with DTOs

--- pagseguro-spring-boot-autoconfigure/src/main/kotlin/com/example/pagseguro/service/PagSeguroRefundService.kt
+++ pagseguro-spring-boot-autoconfigure/src/main/kotlin/com/example/pagseguro/service/PagSeguroRefundService.kt
@@ -1,3 +1,21 @@
 package com.example.pagseguro.service
+
+import com.example.pagseguro.model.refund.*
 import org.springframework.web.client.RestClient
-class PagSeguroRefundService(private val restClient: RestClient)
+import org.springframework.web.client.body
+
+class PagSeguroRefundService(private val restClient: RestClient) {
+
+    fun create(paymentId: String, request: RefundRequest = RefundRequest()): RefundResponse =
+        restClient.post()
+            .uri("/payments/{paymentId}/refunds", paymentId)
+            .body(request)
+            .retrieve()
+            .body<RefundResponse>()!!
+
+    fun get(id: String): RefundResponse =
+        restClient.get()
+            .uri("/refunds/{id}", id)
+            .retrieve()
+            .body<RefundResponse>()!!
+}
