fac773a51b6b29a18a58f644e9505d3e8267cde4
Rodrigo Monteiro <rodrigoma@gmail.com>
2026-03-26 08:36:16 -0300

    chore: add pagbank.kotlin-library convention plugin

---
+++ buildSrc/src/main/kotlin/pagbank.kotlin-library.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.0"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
